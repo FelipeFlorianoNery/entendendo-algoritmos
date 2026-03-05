f0bfb11540f66b685fba2165e90082bdff91ecdb
Felipe Nery <ffneryy@gmail.com>
2026-03-05 20:50:10 -0300

    Adiciona o algoritmo da Pesquisa Binária

--- src/cap01_pesquisa_binaria/PesquisaBinaria.java
+++ src/cap01_pesquisa_binaria/PesquisaBinaria.java
@@ -2,6 +2,35 @@ package cap01_pesquisa_binaria;
 
 public class PesquisaBinaria {
 
+    public static int pesquisaBinaria(int[] lista, int item){
+        int baixo = 0;
+        int alto = lista.length-1;
 
+        while (baixo <= alto){
 
+            int meio = (baixo+alto)/2;
+            int chute = lista[meio];
+
+            if(chute == item){
+                return meio;
+            }
+            if (chute > item){
+                alto = meio-1;
+            }else{
+                baixo = meio+1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] minhaLista = {1,3,5,7,9,11,13,15,18};
+
+        int numeroBuscado = 11;
+
+        int resultado = pesquisaBinaria(minhaLista, numeroBuscado);
+
+        System.out.printf("Índice do número %d: %d%n" , numeroBuscado,resultado);
+    }
 }
